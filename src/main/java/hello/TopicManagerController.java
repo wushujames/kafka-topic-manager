544d83e506b2dff495eda50c9890809e1c34ea7d
James Cheng <wushujames@gmail.com>
2019-02-03 00:41:19 -0800

    Rename class

--- src/main/java/hello/TopicManagerController.java
+++ src/main/java/hello/TopicManagerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class TopicManagerController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
